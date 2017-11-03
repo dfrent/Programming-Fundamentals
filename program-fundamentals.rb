1b0649b404fb07a80d6e51a1107043200ef127a3
Your Name <you@example.com>
2017-11-02 20:22:54 -0400

    program-fundamentals exercise pseudocode

---
+++ program-fundamentals.rb
@@ -0,0 +1,12 @@
+# define a method with percentage as an argument 
+# returns a letter for that percentage
+# ["A","A-","A+", "B","B-","B+", "C","C-","C+", "D","D-","D+", "F"]
+# letter -> percentage 
+
+# def user_score(percentage)
+# 	:grades ["a+"..."f"] = ["100%"..."0%"]
+#    return "a+" for "100%"
+# end
+
+# user_input = input_percent
+# outputs = user_score
